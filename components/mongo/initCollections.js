f0b86f12529e7a23de882ec4876eb9f9a88f9500
feliun <felipe.polo@gmail.com>
2017-05-15 21:19:55 +0200

    Adding connection to mongodb docker container

---
+++ components/mongo/initCollections.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+
+  let collections = {
+    test: null
+  };
+
+  const start = ({ mongo }, cb) => {
+    collections.test = mongo.collection('test');
+    collections.test.createIndex({ id: 1 }, { unique: true, sparse: true });
+
+    mongo.collection('test').insert({ id: Math.floor(Math.random() * 1000) })
+    .then(() => cb(null, collections))
+    .catch(cb);
+  };
+
+  return { start };
+};
