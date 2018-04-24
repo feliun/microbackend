88ecbdf8e4126c69bdca572d14403173b514d8cd
feliun <felipe.polo@gmail.com>
2018-04-24 17:45:04 +0200

    serving bot on html

--- components/routes/api-routes.js
+++ components/routes/api-routes.js
@@ -1,9 +1,19 @@
+const express = require('systemic-express/express');
 const { join } = require('path');
 
 module.exports = () => {
   const start = ({ app }, cb) => {
-    app.get('/', (req, res) => {
-      res.sendFile(join(process.cwd(), 'public', 'views', 'index.html'));
+    app.use(express.static('public'));
+
+    app.get('/tes', (req, res) => {
+      res.sendFile(join(process.cwd(), 'public', 'views', 'tes.html'));
+    });
+
+    app.get('/bot', (req, res) => {
+      res.json({
+        id: process.env.BOT_ID,
+        buttonColor: process.env.BOT_COLOR || '#0000ff',
+      });
     });
 
     cb();
